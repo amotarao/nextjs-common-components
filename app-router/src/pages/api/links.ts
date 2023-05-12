bc940a9c1739cb08fdb4dfb8ccaa9493be33e9f0
Amon Sawamura <amotarao@gmail.com>
2023-05-12 11:11:45 +0200

    add api

---
+++ app-router/src/pages/api/links.ts
@@ -0,0 +1,20 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type Data = {
+  links: {
+    title: string;
+    url: string;
+  }[];
+};
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  const url = 'https://gq9fphyxxi.microcms.io/api/v1/links';
+  const resp = await fetch(url, {
+    headers: {
+      'X-MICROCMS-API-KEY': 'rkoCJHZyH9Qd8GL9AI82j1LQSjZk0jr9lRf5',
+    },
+  });
+  const data = (await resp.json()) as Data;
+
+  res.status(200).json(data);
+}

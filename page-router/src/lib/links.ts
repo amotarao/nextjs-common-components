72fa66f496f4129deb24974ada5f9cb5a237cc0e
Amon Sawamura <amotarao@gmail.com>
2023-05-12 11:22:34 +0200

    use api

--- page-router/src/lib/links.ts
+++ page-router/src/lib/links.ts
@@ -1,7 +1,8 @@
 export const getLinks = async (requestInit?: RequestInit) => {
-  const url = process.env.NEXT_PUBLIC_VERCEL_URL
-    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/links`
-    : 'http://localhost:3000/api/links';
+  const url =
+    process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
+      ? 'https://nextjs-common-components-app-router.vercel.app/api/links'
+      : 'http://localhost:3000/api/links';
   const res = await fetch(url, {
     headers: {
       'X-MICROCMS-API-KEY': 'rkoCJHZyH9Qd8GL9AI82j1LQSjZk0jr9lRf5',

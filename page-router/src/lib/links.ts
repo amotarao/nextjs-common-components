5b988746dbbea75e9d78775b380222912540c588
Amon Sawamura <amotarao@gmail.com>
2023-05-12 11:21:17 +0200

    use api

--- page-router/src/lib/links.ts
+++ page-router/src/lib/links.ts
@@ -1,5 +1,7 @@
 export const getLinks = async (requestInit?: RequestInit) => {
-  const url = 'https://gq9fphyxxi.microcms.io/api/v1/links';
+  const url = process.env.NEXT_PUBLIC_VERCEL_URL
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/links`
+    : 'http://localhost:3000/api/links';
   const res = await fetch(url, {
     headers: {
       'X-MICROCMS-API-KEY': 'rkoCJHZyH9Qd8GL9AI82j1LQSjZk0jr9lRf5',
